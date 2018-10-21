f1ffee91072a49e1006549b705624a5041df56fe
Lukáš Hozda <luk.hozda@gmail.com>
2018-10-21 13:41:10 +0200

    template prep, harmonogram

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,47 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate reqwest;
+extern crate rocket_contrib;
 
+use rocket_contrib::Template;
+use rocket::http::{ContentType, Status};
+use rocket::response::{NamedFile, Response};
+
+use std::io::Cursor;
 use std::path::{PathBuf, Path};
-use rocket::response::NamedFile;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/static/<path..>")]
+pub fn static_server(path: PathBuf) -> Option<NamedFile> {
+	NamedFile::open(Path::new("web/static/").join(path)).ok()
 }
 
-#[get("/static/<file..>")]
-pub fn static_server(file: PathBuf) -> Option<NamedFile> {
-	NamedFile::open(Path::new("web/static/").join(file)).ok()
+#[get("/harmonogram", format = "application/json")]
+pub fn harmonogram<'a>() -> Response<'a> {
+	if let Ok(res) = reqwest::get("http://gsx2json.com/api?id=12Q1jmsBpZh1LHSAcMwXIwWTZwKMzFoypw_fUrDbWJEQ").expect("failed to send request").text() {
+		Response::build()
+			.header(ContentType::JSON)
+			.sized_body(Cursor::new(res))
+			.finalize()
+	} else {
+		Response::build()
+			.status(Status::InternalServerError)
+			.sized_body(Cursor::new("Internal Server Error"))
+			.finalize()
+	}
 }
 
 #[get("/")]
 pub fn index() -> Option<NamedFile> {
-	NamedFile::open(Path::new("web/index.html").join(file)).ok()
+	NamedFile::open(Path::new("web/index.html")).ok()
 }
 
 fn main() {
     rocket::ignite().mount("/", routes![
-    	static_server,
-    	index])
+    		static_server,
+    		harmonogram,
+    		index])
+    	.attach(Template::fairing())
     	.launch();
 }
 
